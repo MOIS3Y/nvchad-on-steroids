0d85fdf7f1a65384acd7c5401d6340749bf853ff
MOIS3Y <stepan@zhukovsky.me>
2024-11-26 20:08:47 +0900

    update: nvchad-ui 3.0

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,9 +1,9 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
+local nvlsp = require "nvchad.configs.lspconfig"
+
 local servers = {
   "bashls",
   "cssls",
@@ -12,30 +12,29 @@ local servers = {
   "emmet_language_server",
   "html",
   "nixd",
-
 }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
 -- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
 }
 
 --python
 lspconfig.pylsp.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
   configurationSources = { 'flake8' },
   settings = {
     plugins = {
