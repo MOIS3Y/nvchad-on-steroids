b07155397ee60b8895b5c41bcdfc9d38a4cd46b2
MOIS3Y <stepan@zhukovsky.me>
2024-11-26 21:01:08 +0900

    fix: chadrc new nvconfig table

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,21 +1,20 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
+M.base46 = {
 	theme = "catppuccin",
   transparency = true,
-
 	hl_override = {
 		["@comment"] = { italic = true },
-	},
+	}
+}
 
-  nvdash = {
-    load_on_startup = true
-  },
+M.nvdash = {
+  load_on_startup = true
 }
 
 return M
