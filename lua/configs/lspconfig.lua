0f31cae43070a3fbca4082cdf3b09d487049d882
MOIS3Y <stepan@zhukovsky.me>
2024-11-27 00:04:08 +0900

    add: json, vue LSP servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -9,7 +9,6 @@ local servers = {
   "cssls",
   "docker_compose_language_service",  -- :set filetype=yaml.docker-compose
   "dockerls",
-  "emmet_language_server",
   "jsonls",
   "html",
   "nixd",
@@ -24,11 +23,59 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- html
+lspconfig.emmet_language_server.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "htmldjango",
+    "less",
+    "sass",
+    "scss",
+    "pug",
+    "typescriptreact",
+    "vue"
+  },
+}
+
 -- typescript
 lspconfig.ts_ls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        -- it's a hack for nixOS /nix/store/
+        location = vim.fs.joinpath(
+          vim.fs.dirname(
+            vim.fs.dirname(
+              vim.fn.system(
+                'echo -n $(readlink -f $(which vue-language-server))'
+              )
+            )
+          ),
+          'lib/node_modules/@vue/language-server'
+        ),
+        --
+        languages = { 'vue' },
+      },
+    },
+  },
+  filetypes = {
+    'typescript',
+    'javascript',
+    'javascriptreact',
+    'typescriptreact',
+    'vue'
+  },
 }
 
 --python
