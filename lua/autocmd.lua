682066a4a28997fc286f5e6f26f9224f986f3315
MOIS3Y <stepan@zhukovsky.me>
2025-05-06 20:42:08 +0900

    feat: open NvDash when all buffers are closed

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,5 +1,16 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+
+-- show Nvdash when all buffers are closed
+autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
 -- fix toggle comment for vala and blueprint
 autocmd("FileType", {
   pattern = { "vala", "blueprint" },
